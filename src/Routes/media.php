050275065b41e1f0806cd71d3ab21b973424df18
Josh Bruce <josh@8fold.pro>
2020-06-14 13:29:32 -0500

    Update media.php

--- src/Routes/media.php
+++ src/Routes/media.php
@@ -8,10 +8,10 @@ Route::prefix("media")->group(function() use ($contentBuilderClass) {
     Route::get("/", function() { abort(404); });
 
     Route::get("/{any}", function($any) use ($contentBuilderClass) {
-        $extension = Shoop::string($image)->divide(".")->last;
+        $extension = Shoop::string($any)->divide(".")->last;
 
         $path = $contentBuilderClass::assetsPathParts()
-            ->plus($contentBuilderClass::domain(), ...Shoop::string($image)->divide("/"))
+            ->plus($contentBuilderClass::domain(), ...Shoop::string($any)->divide("/"))
             ->join("/")->start("/");
 
         return response()->file($path, ["Content-Type: image/{$extension}"]);
