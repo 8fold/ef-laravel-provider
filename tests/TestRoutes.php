1c17302b48c28dae283ee648ae117b9e0c741aa1
Josh Bruce <josh@8fold.pro>
2020-06-27 15:20:09 -0500

    route may not be working

--- tests/TestRoutes.php
+++ tests/TestRoutes.php
@@ -8,3 +8,4 @@ use Eightfold\ShoopExtras\Shoop;
 
 $contentBuilderClass = TestContentBuilder::class;
 include(Shoop::store(__DIR__)->dropLast()->plus("src", "Routes", "any.php")->unfold());
+include(Shoop::store(__DIR__)->dropLast()->plus("src", "Routes", "feed.php")->unfold());
