e305b2cef782ed95d35c177a54899c7aad0b3f51
Josh Bruce <josh@8fold.pro>
2020-07-24 11:04:11 -0500

    Update SiteTracker.php

--- src/SiteTracker.php
+++ src/SiteTracker.php
@@ -71,20 +71,53 @@ class SiteTracker
     private function saveSession()
     {
         $store   = $this->store->plus("sessions", $this->sessionId, $this->time);
+
         $content = Shoop::dictionary([])->plus(
             // store date and time for page view
             Carbon::now("America/Chicago")->format("YmdHisv"), "timestamp",
             // store the page the user came from
             Shoop::string(url()->previous())->minus(request()->root())->unfold(), "previous",
             // store the page the user came to
-            Shoop::string(url()->current())->minus(request()->root())->unfold(), "current"
-        )->json();
-        $store->saveContent($content);
+            Shoop::string(url()->current())->minus(request()->root())->unfold(), "current",
+        );
+
+        // url query parameters
+        $params = Shoop::dictionary([]);
+        Shoop::string(request()->fullUrl())->divide("?", false, 2)
+            ->countIsLessThanOrEqualTo(1,
+                function($result, $divided) use (&$params) {
+                    if ($result->unfold()) { return; }
+                    $divided->last()->divide("&", false)->each(
+                        function($memberValue) use (&$params) {
+                            list($member, $value) = Shoop::string($memberValue)->divide("=", false, 2);
+                            $params = $params->plus($value, $member);
+                        });
+                });
+
+        if ($params->isNotEmpty) {
+            // url had query parameters, used for keyword searches
+            $content = $content->plus($params->object, "params");
+        }
+
+        $store->saveContent($content->json());
     }
 
     private function saveUrl()
     {
-        $store   = $this->store->plus("urls", Hash::make(url()->current()), $this->time);
+        $store   = $this->store->plus(
+            "urls",
+            Shoop::string(url()->current())->minus(request()->root())->isEmpty(
+                function($result, $string) {
+                    return ($result->unfold())
+                        ? "root"
+                        : $string->startsWith("/", function($result, $string) {
+                            return ($result->unfold())
+                                ? $string->dropFirst()
+                                : $string;
+                        });
+                }),
+            $this->time
+        );
         $content = Shoop::dictionary([])->plus(
             // link session
             $this->sessionId, "session",
