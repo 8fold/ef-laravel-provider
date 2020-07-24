2b09a228b6958eefa0337f355d94bbf108c78397
Josh Bruce <josh@8fold.pro>
2020-07-24 09:49:34 -0500

    three tracker areas

--- src/SiteTracker.php
+++ src/SiteTracker.php
@@ -5,6 +5,7 @@ namespace Eightfold\Site;
 use Jaybizzle\CrawlerDetect\CrawlerDetect;
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
 
 use Carbon\Carbon;
 
@@ -20,23 +21,47 @@ use Eightfold\ShoopExtras\{
 
 class SiteTracker
 {
-    static public function saveTransaction(ESStore $store, $sessionId, $hrtime)
+    static public function saveTransaction(ESStore $store, $sessionId, $hrtime): void
     {
-        // web crawler or bot, bail early
-        if ((new CrawlerDetect)->isCrawler()) { return; }
-
-        $sessionId = Type::sanitizeType($sessionId, ESString::class);
         $hrtime = Type::sanitizeType($hrtime, ESString::class);
 
-        $store   = $store->plus($sessionId, $hrtime);
-        $content = Shoop::dictionary([])->plus(
-            // store date and time for page view
-            Carbon::now("America/Chicago")->format("YmdHisv"), "timestamp",
-            // store the page the user came from
-            Shoop::string(url()->previous())->minus(request()->root())->unfold(), "previous",
-            // store the page the user came to
-            Shoop::string(url()->current())->minus(request()->root())->unfold(), "current"
-        )->json();
-        $store->saveContent($content);
+
+        $detect = new CrawlerDetect;
+        if ($detect->isCrawler()) {
+            // presumed web crawler or bot, get name
+            $crawler = $detect->matches();
+            if ($crawler !== null) {
+                $store   = $store->plus("crawlers", Hash::make($crawler), $hrtime);
+                $content = Shoop::dictionary([])->plus(
+                    // store name of crawler
+                    $crawler, "crawler"
+                )->json();
+                $store->storeContent($content);
+            }
+
+        } elseif (! $detect->isCrawler()) {
+            // presumed human
+            $sessionId = Type::sanitizeType($sessionId, ESString::class);
+
+            $store   = $store->plus("sessions", $sessionId, $hrtime);
+            $content = Shoop::dictionary([])->plus(
+                // store date and time for page view
+                Carbon::now("America/Chicago")->format("YmdHisv"), "timestamp",
+                // store the page the user came from
+                Shoop::string(url()->previous())->minus(request()->root())->unfold(), "previous",
+                // store the page the user came to
+                Shoop::string(url()->current())->minus(request()->root())->unfold(), "current"
+            )->json();
+            $store->saveContent($content);
+
+            $store   = $store->plus("urls", Hash::make(url()->current()), $hrtime);
+            $content = Shoop::dictionary([])->plus(
+                // link session
+                $sessionId, "session",
+                // link time
+                $hrtime, "timestamp"
+            )->json();
+            $store->saveContent($content);
+        }
     }
 }
